c1d69fe570fc65b4e68027a792a527e7c41d0017
makersmecca <37550248+makersmecca@users.noreply.github.com>
2021-09-08 21:57:31 +0530

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,18 +1,6 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBbxf_UzMXbo0w47qlIaghwyBoHY1WOfg0",
-    authDomain: "counter-99162.firebaseapp.com",
-    projectId: "counter-99162",
-    storageBucket: "counter-99162.appspot.com",
-    messagingSenderId: "640170582989",
-    databaseURL:"https://wireless-database-4a51b-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    appId: "1:640170582989:web:fc7509f83490077eb31a84"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//val percentage value
+
 let val;
 val=86
-
 $(document).ready(function() {
   
     $(".my-progress-bar").circularProgress({
@@ -25,6 +13,19 @@ $(document).ready(function() {
         text: ""
     }).circularProgress('animate', val, 1000);
 });
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBbxf_UzMXbo0w47qlIaghwyBoHY1WOfg0",
+    authDomain: "counter-99162.firebaseapp.com",
+    projectId: "counter-99162",
+    storageBucket: "counter-99162.appspot.com",
+    messagingSenderId: "640170582989",
+    databaseURL:"https://wireless-database-4a51b-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    appId: "1:640170582989:web:fc7509f83490077eb31a84"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//val percentage value
 
 
 
